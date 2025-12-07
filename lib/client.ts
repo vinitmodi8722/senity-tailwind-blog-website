9ce650d7489bc608df8375c93059468f454adb7c
modi vinit j. <vinit.modi8722@gmail.com>
2025-12-07 16:01:21 +0530

    update

--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,10 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from "../sanity/env";
+import { apiVersion, dataset, projectId } from '../sanity/env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
-});
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+})
